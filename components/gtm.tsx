59f6070ee8222facc3ed582a599162282c44737d
Jeevan Kumar <g1mishra.dev@gmail.com>
2025-02-14 13:40:56 +0530

    final: fix build error

--- components/gtm.tsx
+++ components/gtm.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 import { sendGAEvent } from "@next/third-parties/google";
 
 export const GTM_PageView = () => {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    const search = searchParams.toString();
+    const search = window.location.search;
     const fullPath = search ? `${pathname}?${search}` : pathname;
 
     sendGAEvent("event", "page_view", {
       page_path: fullPath,
     });
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null;
 };
